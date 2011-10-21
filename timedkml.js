9716c54fb86007537e3e0e60922d32ffa2f50fcf
Schuyler Duveen <sky@skyb.us>
2011-10-21 13:02:44 -0400

    play button

--- timedkml.js
+++ timedkml.js
@@ -17,41 +17,69 @@ TimedKML.prototype = {
             div: opts.map.html_id,
             allOverlays: true
         });
-        this.bg_layers = [];
         this.layers = [];
         //var osm = new OpenLayers.Layer.OSM();
         //layers.push(osm);
         var gmap;
         if (opts.map.mapping_service=='google') {
-            gmap = new OpenLayers.Layer.Google("Google Streets", 
+            this.bg_layer = new OpenLayers.Layer.Google("Google Streets", 
                                                {visibility: true});
-            this.bg_layers.push(gmap);
         }
         this.addSequence(opts.sequence);
         this.createSlider(opts);
         // note that first layer must be visible
-        this.map.addLayers(this.bg_layers);
+        this.map.addLayers([this.bg_layer]);
         this.map.addLayers(this.layers);
 
         //this.map.addControl(new OpenLayers.Control.LayerSwitcher());
-        this.map.zoomToMaxExtent();
-
+        //this.map.zoomToMaxExtent();
+        var center = opts.map.center;
+        if (center) {
+            console.log(center);
+            this.map.setCenter(new OpenLayers.LonLat(center.lon, center.lat), 
+                               opts.map.zoom );
+        }
+        this.next(0);
         return this;
+    },
+    play:function(reset,speed) {
+        var self = this;
+        this.keep_playing = true;
+        speed = speed || 1000;
+        var start = (reset) ? -1 : this.cur_index;
+        var timedNext = function() {
+            console.log(start);
+            if (self.keep_playing && start<self.layers.length-1) {
+                self.next(++start);
+                setTimeout(timedNext,speed);
+            }
+        }
+        timedNext();
+    },
+    next:function(ind) {
+        ind = ind || 0;
+        if (this.current_layer) {
+            this.current_layer.setVisibility(false);
+        }
+        this.cur_index = ind;
+        this.current_layer = this.layers[ind];
+        this.current_layer.setVisibility(true);
+        this.slider.slider('value',ind);
     },
     createSlider:function(opts) {
         var self = this;
-        var slider = $('#'+opts.slider.html_id).slider({
+        this.slider = $('#'+opts.slider.html_id).slider({
 	    'min':0, 'max':opts.sequence.length-1, 'value':0,
 	    slide: function( event, ui ) {
-                //hide current layer
-                //show
-                if (self.current_layer) {
-                    self.current_layer.setVisibility(false);
-                }
-                self.current_layer = self.layers[ui.value];
-                self.current_layer.setVisibility(true);
+                self.keep_playing = false;
+                self.next(ui.value);
 	    }
 	});
+        if (opts.slider.play_button_id) {
+            $('#'+opts.slider.play_button_id).click(function() {
+                self.play(true);
+            });
+        }
     },
     addSequence:function(seq) {
         for (var i=0,l=seq.length;i<l;i++) {
